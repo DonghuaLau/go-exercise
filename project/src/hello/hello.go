f425c01b455d33f21af3b0371706a5f924affc11
Donghua.Lau <me@liudonghua.net>
2017-09-11 11:42:46 +0800

    workspace sample

---
+++ project/src/hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"com/stringutil"
+)
+
+func main() {
+	fmt.Printf("Hello, world.\n")
+	fmt.Println(stringutil.Reverse("!oG, olleH"))
+}
---
+++ project/src/main/enter.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"com/stringutil"
+)
+
+func main() {
+	fmt.Printf("Another main enter\n")
+	fmt.Println(stringutil.Reverse("!oG, olleH"))
+}
