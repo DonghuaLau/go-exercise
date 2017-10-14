d52098ce372c7b5afbb98893fcfa02a7031ff5ad
Donghua.Lau <me@liudonghua.net>
2017-10-14 13:22:44 +0800

    update code

--- project/src/webapp/Routes.go
+++ project/src/webapp/Routes.go
@@ -10,9 +10,12 @@ type Router struct {
 	_uri     string
 	_handler func(http.ResponseWriter, *http.Request)
 }
+
+/*
 	app._routers = []Router{
 		{"GET", "/index", controllers.HomeController.Index},
 		{"GET", "/profile", controllers.HomeController.Profile},
 		{"GET", "/user/index", controllers.UserController.Index},
 		{"GET", "/user/profile", controllers.UserController.Profile},
 	}
+*/
